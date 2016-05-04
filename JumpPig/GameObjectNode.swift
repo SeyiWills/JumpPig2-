d8992f8bd20ff3b319143cfddb4acced71c3b61b
Seyi Wiliams <SeyiWills97@gmail.com>
2016-05-04 10:32:33 -0400

    almost final.

---
+++ JumpPig/GameObjectNode.swift
@@ -0,0 +1,88 @@
+//
+//  GameObjectNode.swift
+//  MegaJump
+//
+//  Created by Student on 5/3/16.
+//  Copyright © 2016 JoeMendoza. All rights reserved.
+//
+
+import SpriteKit
+
+enum StarType: Int {
+    case Normal = 0
+    case Special
+}
+
+struct CollisionCategoryBitmask {
+    static let Player: UInt32 = 0x00
+    static let Star: UInt32 = 0x01
+    static let Platform: UInt32 = 0x02
+}
+
+enum PlatformType: Int {
+    case Normal = 0
+    case Break
+}
+
+class GameObjectNode: SKNode {
+    func collisionWithPlayer(player: SKNode) -> Bool {
+        return false
+    }
+    
+    func checkNodeRemoval(playerY: CGFloat) {
+        if playerY > self.position.y + 300.0 {
+            self.removeFromParent()
+        }
+    }
+}
+
+class StarNode: GameObjectNode {
+    
+    let starSound = SKAction.playSoundFileNamed("StarPing.wav", waitForCompletion: false)
+    
+    var starType: StarType!
+    
+    override func collisionWithPlayer(player: SKNode) -> Bool {
+        // Boost the player up
+        player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 400.0)
+        
+        // Remove this Star
+        // Play sound
+        runAction(starSound, completion: {
+            // Remove this Star
+            self.removeFromParent()
+        })
+        
+        // Award score
+        GameState.sharedInstance.score += (starType == .Normal ? 20 : 100)
+        
+        // Award stars
+        GameState.sharedInstance.stars += (starType == .Normal ? 1 : 5)
+        
+        // The HUD needs updating to show the new stars and score
+        return true
+    }
+}
+
+class PlatformNode: GameObjectNode {
+    var platformType: PlatformType!
+    
+    override func collisionWithPlayer(player: SKNode) -> Bool {
+        // 1
+        // Only bounce the player if he's falling
+        if player.physicsBody?.velocity.dy < 0 {
+            // 2
+            player.physicsBody?.velocity = CGVector(dx: player.physicsBody!.velocity.dx, dy: 250.0)
+            
+            // 3
+            // Remove if it is a Break type platform
+            if platformType == .Break {
+                self.removeFromParent()
+            }
+        }
+        
+        // 4
+        // No stars for platforms
+        return false
+    }
+}
--- JumpPig/GameViewController.swift
+++ JumpPig/GameViewController.swift
@@ -1,9 +1,9 @@
 //
 //  GameViewController.swift
-//  JumpPig
+//  MegaJump
 //
-//  Created by Oluwaseyi Williams on 5/4/16.
-//  Copyright (c) 2016 Oluwaseyi Williams. All rights reserved.
+//  Created by Student on 5/3/16.
+//  Copyright (c) 2016 JoeMendoza. All rights reserved.
 //
 
 import UIKit
@@ -13,21 +13,16 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        if let scene = GameScene(fileNamed:"GameScene") {
-            // Configure the view.
-            let skView = self.view as! SKView
-            skView.showsFPS = true
-            skView.showsNodeCount = true
-            
-            /* Sprite Kit applies additional optimizations to improve rendering performance */
-            skView.ignoresSiblingOrder = true
-            
-            /* Set the scale mode to scale to fit the window */
-            scene.scaleMode = .AspectFill
-            
-            skView.presentScene(scene)
-        }
+        
+        let skView = self.view as! SKView
+        
+        skView.showsFPS = true
+        skView.showsNodeCount = true
+        
+        let scene = GameScene(size: skView.bounds.size)
+        scene.scaleMode = .AspectFit
+        
+        skView.presentScene(scene)
     }
 
     override func shouldAutorotate() -> Bool {
